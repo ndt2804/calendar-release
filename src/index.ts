5cc4bb5c2130f33c85e8570022e5d7e0fedf78ef
ndt2804 <nduytan.uit.a3@gmail.com>
2023-10-08 00:22:14 +0700

    feat: done release calenader 1 days

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
-console.log("Hello via Bun!");
-import { crawlData } from "./services/data";
-(async () => {
-    const targetDate = '2023-10-02';
-    try {
-        const result = await crawlData(targetDate);
-        console.log('Result:', result);
-    } catch (error) {
-        console.error('Lỗi:', error);
+import { Collection, Client, GatewayIntentBits, Message, TextChannel } from 'discord.js';
+import * as dotenv from 'dotenv';
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
+import vi from "dayjs/locale/vi.js";
+import customParseFormat from "dayjs/plugin/customParseFormat.js";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
+dayjs.locale(vi);
+dayjs.tz.setDefault("Asia/Ho_Chi_Minh");
+dotenv.config();
+
+
+
+import { release } from "./services/data";
+
+
+if (!process.env.channleID)
+    throw new Error("Releases channel is not defined.");
+const channleAnouments = process.env.channleID;
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent],
+});
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}`);
+    client.user?.setActivity('Reading data for calendar release books')
+});
+// const date = dayjs.tz().startOf("day");
+const date = dayjs.tz("2023-10-02", "YYYY-MM-DD", "Asia/Ho_Chi_Minh");
+
+client.on('messageCreate', async (message) => {
+
+    if (message.content == '!crawl') {
+        const response = await release(date);
+        if (!response) return;
+        const channel = client.channels.cache.get(channleAnouments) as TextChannel;
+        if (channel) channel.send({ embeds: [response.embed] });
     }
-})();
+});
+
+client.login(process.env.discordToken || 'Your Token');
--- src/services/data.ts
+++ src/services/data.ts
@@ -1,36 +1,55 @@
 import * as cheerio from 'cheerio';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
+import { EmbedBuilder } from "@discordjs/builders";
+import { Dayjs } from "dayjs";
 dotenv.config();
 const url = process.env.url || 'https://default-url.com';
-export async function crawlData(targetDate: string): Promise<string[]> {
-    try {
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
-
-        const releaseDays = $('.release-day');
-        const result: string[] = [];
-
-        releaseDays.each((index: number, element: cheerio.Element) => {
-            const id = $(element).attr('id');
-            if (id === targetDate) {
-                const items = $(element).find('.grid-cols-2');
-
-                items.each((i: number, item: cheerio.Element) => {
-                    const titleElement = $(item).find('.decoration-primary-400.font-condensed.text-xl.font-black');
-                    const title = titleElement.text().trim();
-
-                    if (title) {
-                        result.push(title);
-                    }
-                });
-            }
-        });
-
-        return result;
-    } catch (error) {
-        console.error('Lỗi khi tải trang web:', error);
-        return [];
+export async function getReleases(date: Dayjs): Promise<string[]> {
+    const { data: html } = await axios.get(url);
+    const $ = cheerio.load(html);
+    const result: string[] = [];
+    const dateString = date.format('YYYY-MM-DD');
+    $('.release-day').each((i, element) => {
+        const id = $(element).attr('id');
+        if (id === dateString) {
+            $(element).find('.group').each((i, index) => {
+                const title = $(index).find('.space-y-1 > div:nth-child(1)').text().trim();
+                const volume = $(index).find('.space-y-1 > div:nth-child(2)').text().trim();
+                const books = `${title} - ${volume}`;
+                result.push(books);
+            });
+        }
+    });
+    return result;
+}
+export async function release(date: Dayjs) {
+    const releases = await getReleases(date);
+    if (!releases || releases.length === 0) {
+        console.log('Không có thông tin phát hành cho ngày này.');
+        return;
     }
+    const releaseText = releases.join('\n');
+    const embed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle('Lịch Phát Hành')
+        .setURL('https://tana.moe/calendar')
+        .setAuthor({
+            name: "Tana.moe",
+            iconURL: "https://tana.moe/apple-touch-icon.png",
+            url: "https://tana.moe",
+        })
+        .setDescription(
+            Intl.DateTimeFormat("vi-VN", {
+                dateStyle: "full",
+                timeZone: "Asia/Ho_Chi_Minh",
+            }).format(date.toDate()),
+        )
+        .addFields(
+            {
+                name: 'Hôm nay có:',
+                value: releaseText
+            },
+        );
+    return { embed };
 }
